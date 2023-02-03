4dce1b2f8981765c1e77bd21e84c9bc37496ddc3
Alireza <karayyb@gmail.com>
2023-02-03 12:53:24 +0330

    fix bug inquiry_contract_screen.dart

--- lib/screens/verify_contract/inquiry_contract_screen.dart
+++ lib/screens/verify_contract/inquiry_contract_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:siraf3/widgets/block_btn.dart';
 import 'package:siraf3/widgets/my_card.dart';
 import 'package:siraf3/widgets/simple_app_bar.dart';
@@ -11,12 +12,18 @@ class InquiryContractScreen extends StatefulWidget {
 }
 
 class _InquiryContractScreen extends State<InquiryContractScreen> {
+  TextEditingController contractNumberController = TextEditingController();
+  TextEditingController contractDateController = TextEditingController();
+  TextEditingController officeAgentIdController = TextEditingController();
+  TextEditingController numberPhoneController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Themes.background,
       appBar: SimpleAppBar(titleText: "استعلام قرارداد"),
       body: Stack(
+        fit: StackFit.expand,
         children: [
           Padding(
             padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
@@ -27,7 +34,10 @@ class _InquiryContractScreen extends State<InquiryContractScreen> {
                   title: "شماره قرارداد",
                   padding: EdgeInsets.symmetric(vertical: 15, horizontal: 12),
                   child: TextField2(
+                    controller: contractNumberController,
                     style: TextStyle(fontSize: 14, color: Themes.text),
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.number,
                     decoration: InputDecoration.collapsed(
                       hintText: "مثال: 21345000976",
                       hintStyle: TextStyle(
@@ -41,7 +51,9 @@ class _InquiryContractScreen extends State<InquiryContractScreen> {
                   title: "تاریخ عقد قرارداد",
                   padding: EdgeInsets.symmetric(vertical: 15, horizontal: 12),
                   child: TextField2(
+                    controller: contractDateController,
                     style: TextStyle(fontSize: 14, color: Themes.text),
+                    textInputAction: TextInputAction.next,
                     decoration: InputDecoration.collapsed(
                       hintText: "مثال: 1401/09/14",
                       hintStyle: TextStyle(
@@ -55,7 +67,10 @@ class _InquiryContractScreen extends State<InquiryContractScreen> {
                   title: "شناسه صنفی دفتر املاک",
                   padding: EdgeInsets.symmetric(vertical: 15, horizontal: 12),
                   child: TextField2(
+                    controller: officeAgentIdController,
                     style: TextStyle(fontSize: 14, color: Themes.text),
+                    textInputAction: TextInputAction.next,
+                    keyboardType: TextInputType.number,
                     decoration: InputDecoration.collapsed(
                       hintText: "مثال: 2241000452",
                       hintStyle: TextStyle(
@@ -67,29 +82,37 @@ class _InquiryContractScreen extends State<InquiryContractScreen> {
                 MyCard(
                   margin: const EdgeInsets.symmetric(vertical: 8),
                   title: "شماره موبایل یکی از طرفین قرارداد",
-                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 12),
+                  padding: EdgeInsets.only(top: 12, bottom: 12, right: 12, left: 5),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      TextField2(
-                        style: TextStyle(fontSize: 14, color: Themes.text),
-                        decoration: InputDecoration.collapsed(
-                          hintText: "مثال: 09123333333",
-                          hintStyle: TextStyle(
-                            color: Colors.grey.shade400,
+                      Expanded(
+                        child: TextField2(
+                          controller: numberPhoneController,
+                          style: TextStyle(fontSize: 14, color: Themes.text),
+                          textInputAction: TextInputAction.done,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration.collapsed(
+                            hintText: "مثال: 09123333333",
+                            hintStyle: TextStyle(
+                              color: Colors.grey.shade400,
+                            ),
                           ),
                         ),
                       ),
                       InkWell(
                         onTap: verifyNumberPhone,
                         borderRadius: BorderRadius.circular(20),
-                        child: Text(
-                          "تایید شماره",
-                          style: TextStyle(
-                            color: Themes.blue,
-                            fontSize: 13,
-                            fontWeight: FontWeight.bold,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 12),
+                          child: Text(
+                            "تایید شماره",
+                            style: TextStyle(
+                              color: Themes.blue,
+                              fontSize: 11,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       )
@@ -103,12 +126,19 @@ class _InquiryContractScreen extends State<InquiryContractScreen> {
             bottom: 0,
             left: 0,
             right: 0,
-            child: BlockBtn(text: "استعلام قرارداد", onTap: () {}),
+            child: BlockBtn(text: "استعلام قرارداد", onTap: inquiryContract),
           ),
         ],
       ),
     );
   }
 
-  void verifyNumberPhone() {}
+  //event listeners
+  void verifyNumberPhone() {
+    //todo: implement event listener
+  }
+
+  void inquiryContract() {
+    //todo: implement event listener
+  }
 }
