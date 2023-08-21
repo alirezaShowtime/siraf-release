a5e5d5d36268f1be06a12566161937dcaa58a551
amir akbari <karayyb@gmail.com>
2023-08-21 14:53:40 +0330

    file image screen is changed

--- lib/screens/file_images_screen.dart
+++ lib/screens/file_images_screen.dart
@@ -1,12 +1,12 @@
+import 'package:siraf3/extensions/string_extension.dart';
+import 'package:siraf3/widgets/my_back_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
-import 'package:siraf3/models/file_detail.dart';
-import 'package:siraf3/widgets/my_back_button.dart';
 
 class FileImagesScreen extends StatefulWidget {
-  FileDetail file;
+  var file;
   int? index;
 
   FileImagesScreen({required this.file, this.index, Key? key}) : super(key: key);
@@ -16,14 +16,20 @@ class FileImagesScreen extends StatefulWidget {
 }
 
 class _FileImagesScreenState extends State<FileImagesScreen> {
+  var imageTitleSetState;
+
+  int currentImageIndex = 0;
+
   @override
   void initState() {
     super.initState();
+
+    currentImageIndex = widget.index ?? 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
+    return AnnotatedRegion(
       value: SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
         statusBarBrightness: Brightness.light,
@@ -38,18 +44,18 @@ class _FileImagesScreenState extends State<FileImagesScreen> {
           children: [
             PhotoViewGallery.builder(
               scrollPhysics: const BouncingScrollPhysics(),
+              onPageChanged: (i) => imageTitleSetState?.call(() => currentImageIndex = i),
               builder: (BuildContext context, int index) {
                 return PhotoViewGalleryPageOptions(
-                  // maxScale: 4.0,
-                  // minScale: 0.5,
-                  minScale: 0.1,
-                  imageProvider: NetworkImage(widget.file.media!.images![index].path!),
-                  initialScale: PhotoViewComputedScale.contained,
-                  heroAttributes: PhotoViewHeroAttributes(tag: widget.file.media!.images![index].id!),
+                  maxScale: 0.18,
+                  minScale: 0.18,
+                  imageProvider: NetworkImage(widget.file.media!.image![index].path!),
+                  initialScale: PhotoViewComputedScale.covered,
+                  heroAttributes: PhotoViewHeroAttributes(tag: widget.file.media!.image![index].id!),
                 );
               },
               pageController: PageController(initialPage: widget.index ?? 0),
-              itemCount: widget.file.media!.images!.length,
+              itemCount: widget.file.media!.image!.length,
               loadingBuilder: (context, event) => Center(
                 child: Container(
                   width: 20.0,
@@ -68,16 +74,45 @@ class _FileImagesScreenState extends State<FileImagesScreen> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   MyBackButton(color: Colors.white),
-                  SizedBox(width: 10,),
+                  SizedBox(width: 15),
                   Text(
-                    widget.file.name ?? '',
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 15,
-                    ),
+                    widget.file.name ?? "ناشناس",
+                    style: TextStyle(fontSize: 15, color: Colors.white),
                   ),
                 ],
               ),
+            ),
+            StatefulBuilder(
+              builder: (context, setState) {
+                imageTitleSetState = setState;
+
+                String title = widget.file.media?.image?[currentImageIndex].name;
+
+                if (!title.isFill()) return SizedBox();
+
+                return Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    margin: EdgeInsets.only(left: 10, right: 10, top: 100),
+                    constraints: BoxConstraints(minWidth: 50),
+                    decoration: BoxDecoration(
+                      color: Colors.white10,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Text(
+                      title,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontFamily: "IranSansMedium",
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
           ],
         ),
